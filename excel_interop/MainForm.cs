77fd5a95624ed01208582198ca629175e56a3ede
Tom Gregor <tgregor@ivsoftware.com>
2022-07-14 16:59:00 -0600

    Added wait cursor

--- excel_interop/MainForm.cs
+++ excel_interop/MainForm.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Range = Microsoft.Office.Interop.Excel.Range;
 using Application = Microsoft.Office.Interop.Excel.Application;
+using Range = Microsoft.Office.Interop.Excel.Range;
 
 namespace excel_interop
 {
@@ -39,8 +32,13 @@ namespace excel_interop
 
         private void checkBoxExcelVisible_CheckedChanged(object sender, EventArgs e)
         {
-            _xlApp.Visible = checkBoxExcelVisible.Checked; 
-            checkBoxWorkbookOpen.Visible = _xlApp.Visible;
+            Cursor = Cursors.WaitCursor;
+            BeginInvoke((MethodInvoker)delegate 
+            {
+                _xlApp.Visible = checkBoxExcelVisible.Checked; 
+                checkBoxWorkbookOpen.Visible = _xlApp.Visible;
+                Cursor = Cursors.Default;
+            });
         }
 
         private void checkBoxWorkbookOpen_CheckedChanged(object sender, EventArgs e)
