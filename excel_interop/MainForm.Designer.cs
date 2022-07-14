b3fd283dc4fe061d895b2f5127eaeff568d40767
Tom Gregor <tgregor@ivsoftware.com>
2022-07-14 16:55:10 -0600

    Add project files.

---
+++ excel_interop/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace excel_interop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBoxExcelVisible = new System.Windows.Forms.CheckBox();
+            this.checkBoxWorkbookOpen = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(31, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(518, 261);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBoxExcelVisible
+            // 
+            this.checkBoxExcelVisible.AutoSize = true;
+            this.checkBoxExcelVisible.Location = new System.Drawing.Point(36, 40);
+            this.checkBoxExcelVisible.Name = "checkBoxExcelVisible";
+            this.checkBoxExcelVisible.Size = new System.Drawing.Size(132, 29);
+            this.checkBoxExcelVisible.TabIndex = 2;
+            this.checkBoxExcelVisible.Text = "Excel Visible";
+            this.checkBoxExcelVisible.UseVisualStyleBackColor = true;
+            this.checkBoxExcelVisible.CheckedChanged += new System.EventHandler(this.checkBoxExcelVisible_CheckedChanged);
+            // 
+            // checkBoxWorkbookOpen
+            // 
+            this.checkBoxWorkbookOpen.AutoSize = true;
+            this.checkBoxWorkbookOpen.Location = new System.Drawing.Point(209, 40);
+            this.checkBoxWorkbookOpen.Name = "checkBoxWorkbookOpen";
+            this.checkBoxWorkbookOpen.Size = new System.Drawing.Size(171, 29);
+            this.checkBoxWorkbookOpen.TabIndex = 2;
+            this.checkBoxWorkbookOpen.Text = "Workbook Open";
+            this.checkBoxWorkbookOpen.UseVisualStyleBackColor = true;
+            this.checkBoxWorkbookOpen.Visible = false;
+            this.checkBoxWorkbookOpen.CheckedChanged += new System.EventHandler(this.checkBoxWorkbookOpen_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 384);
+            this.Controls.Add(this.checkBoxWorkbookOpen);
+            this.Controls.Add(this.checkBoxExcelVisible);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Excel Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBoxExcelVisible;
+        private System.Windows.Forms.CheckBox checkBoxWorkbookOpen;
+    }
+}
+
---
+++ excel_interop/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Range = Microsoft.Office.Interop.Excel.Range;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace excel_interop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            _xlApp = new Microsoft.Office.Interop.Excel.Application();            
+        }
+        private readonly Application _xlApp;
+        private Workbook _xlBook = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+                _xlBook?.Close();
+                _xlApp.Quit();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void checkBoxExcelVisible_CheckedChanged(object sender, EventArgs e)
+        {
+            _xlApp.Visible = checkBoxExcelVisible.Checked; 
+            checkBoxWorkbookOpen.Visible = _xlApp.Visible;
+        }
+
+        private void checkBoxWorkbookOpen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxWorkbookOpen.Checked)
+            {
+                var filePath = System.IO.Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "Excel",
+                    "TestCells.xlsx");
+
+                _xlBook = _xlApp.Workbooks.Open(filePath);
+                Worksheet xlSheet = _xlBook.Sheets["Sheet1"];
+                Range xlRange = xlSheet.Range["A1", "B4"];
+
+                textBox1.Clear();
+                for (int i = 1; i <= xlRange.Rows.Count; i++)
+                {
+                    var line = new List<string>();
+                    for (int j = 1; j <= xlRange.Columns.Count; j++)
+                    {
+                        Range range = xlRange.Cells[i, j];
+                        line.Add(range.Value2);
+                    }
+                    textBox1.AppendText(string.Join(" | ", line));
+                    textBox1.AppendText(Environment.NewLine);
+                }
+            }
+            else
+            {
+                _xlBook?.Close();
+                _xlBook = null;
+            }
+        }
+    }
+}
